89fe1ef1173d6082ac8cae9ab41694277979e9fb
Lammmmmmm <ozc236@163.com>
2020-01-02 21:11:15 +0800

    alloctor.h and construct.h

---
+++ allocator.h
@@ -0,0 +1,103 @@
+#if !defined(MYSTL_ALLOCATOR_H)
+#define MYSTL_ALLOCATOR_H
+
+#include "construct.h"
+
+namespace mystl
+{
+// 一个简单的 alloctor 模板类，仅仅是对 :operator new 和 :operator delete的简单封装
+template <typename T>
+class allocator {
+public:
+    // STL 要求的类型别名定义
+    using value_type        = T;
+    using pointer           = T*;
+    using const_pointer     = const T*;
+    using reference         = T&;
+    using const_reference   = const T&;
+    using size_type         = size_t;
+    using difference_type   = ptrdiff_t;
+public:
+    // STL 要求的类接口，使用静态函数实现可以使频繁调用下减小开销
+	// 负责分配内存
+	static T*	allocate();
+	static T*	allocate(size_type n);
+	// 负责释放内存
+	static void deallocate(T* ptr);
+	static void deallocate(T*, size_type n);
+	// 负责构造对象
+	static void construct(T* ptr);
+	static void construct(T* ptr, const T& value);
+	static void construct(T* ptr, T&& value);
+	// 负责析构对象
+	static void destroy(T* ptr);
+	static void destroy(T* first, T* last);
+	// 获取某对象的地址
+    static T*       address(T& val);
+    // 获取可配置T类型对象的最大数目
+    static size_t   max_size();
+	// 使T类型的alloctor可以为U类型的对象分配内存
+    template<typename U>
+    struct rebind {
+        using other = allocator<U>;
+    };  
+};
+ 
+template <typename T>
+T* allocator<T>::allocate() {
+	return static_cast<T*>(::operator new(sizeof(T)));
+}
+
+template <typename T>
+T* allocator<T>::allocate(size_type n) {
+	return static_cast<T*>(::operator new(n * sizeof(T)));
+}
+
+template <typename T>
+void allocator<T>::deallocate(T* ptr) {
+	if (ptr != nullptr) ::operator delete(ptr);
+}
+
+template <typename T>
+void allocator<T>::deallocate(T* ptr, size_type) {
+	if (ptr != nullptr) ::operator delete(ptr);
+}
+
+template <typename T>
+void allocator<T>::construct(T* ptr) {
+	mystl::construct(ptr);
+}
+
+template <typename T>
+void allocator<T>::construct(T* ptr, const T& value) {
+	mystl::construct(ptr, value);
+}
+
+template <typename T>
+void allocator<T>::construct(T* ptr, T&& value) {
+	mystl::construct(ptr, mystl::move(value));
+}
+
+template <typename T>
+void allocator<T>::destroy(T* ptr) {
+	mystl::destroy(ptr);
+}
+
+template <typename T>
+void allocator<T>::destroy(T* first, T* last) {
+	mystl::destroy(first, last);
+}
+
+template <typename T>
+T* allocator<T>::address(T& val) {
+    return (T*)&val;
+}
+
+template <typename T>
+size_t allocator<T>::max_size() {
+    return size_t(UINT_MAX/sizeof(T));
+}
+// TODO : 具有 SGI STL特色的两级分配器
+} // namespace mystl
+
+#endif // MYSTL_ALLOCATOR_H
---
+++ construct.h
@@ -0,0 +1,42 @@
+#if !defined(MYSTL_CONSTRUCT_H)
+#define MYSTL_CONSTRUCT_H
+
+#include <new> // placement new 在此头文件内
+#include "typetraits.h"
+
+namespace mystl
+{
+// 全局 construct 函数，使用 placement new 将一个值放入指针所指内存
+// 使用了两个模板参数，并使用显式转换
+template<typename T1, typename T2>
+inline void construct(T1* ptr, const T2& value) { new(ptr) T1(value); }
+
+template<typename T1, typename T2>
+inline void construct(T1* ptr, T2&& value) { new(ptr) T1(value); }
+
+template<typename T>
+inline void construct(T* ptr) { new(ptr) T(); }
+// 单个参数的全局 destroy 函数，直接调用对象的析构函数
+template<typename T>
+inline void destroy(T* ptr) { ptr ->~T(); }
+
+template<typename ForwardIterator>
+inline void destroy_aux(ForwardIterator first, ForwardIterator last, _true_type) { }
+
+template<typename ForwardIterator>
+inline void destroy_aux(ForwardIterator first, ForwardIterator last, _false_type) {
+    for(; first != last; ++first)
+        destroy(first);
+}
+
+// 两个参数的全局 destroy 函数，根据其是否具有 trivial 析构函数进行重载
+template<typename ForwardIterator>
+inline void destroy(ForwardIterator first, ForwardIterator last) {
+    using is_trivial_dtor = type_traits<value_type(first)()>::has_trivial_destructor;
+    destroy_aux(first, last, is_trivial_dtor());
+}
+
+} // namespace mystl
+
+
+#endif // MYSTL_CONSTRUCT_H

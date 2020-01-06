6b241f9b971b0bce0b03016c101834fd56747db5
Lammmmmmm <ozc236@163.com>
2020-01-06 23:58:50 +0800

    运行 test_alloc( ) 的 main( ) 程序

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include "allocator.h"
+#include "alloc.h"
 #include "type_traits.h"
 #include <vector>
+#include "test_alloc.h"
 
 using std::cout;
 using std::endl;
+using FunPtr = void (*)(int&);
+void bigger(int& num){
+    num += 1;
+}
+void (* set_m(FunPtr))(){
+
+}
+
+union obj {
+    union obj* next;
+    char data[1];
+};
 
 int main(){
-    std::vector<int,mystl::allocator<int>> vec(5,5);
-    for(auto &each: vec)
-        cout << each << " ";
-    cout << endl;
+    mystl::test_alloc();
     return 0;
 }
